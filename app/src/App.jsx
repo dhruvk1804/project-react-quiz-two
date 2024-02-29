afe6e4d51f39eb0ad391308a056edef530118510
dhruvk1804 <dhruvk1804@gmail.com>
2024-02-29 23:00:55 +0530

    done

---
+++ app/src/App.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import QuizComponent from "./components/QuizComponent"
+export default class App extends Component{
+  render(){
+    return(
+      <div>
+        {/* <HomeComponent/> */}
+        <QuizComponent/>
+        {/* <ResultComponent/> */}
+      </div>
+    );
+  };
+}
